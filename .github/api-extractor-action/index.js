ac96fc2bdd2c40d7f0b0235fa8933a7078343881
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-03 08:23:05 +0200

    trigger action

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -4,7 +4,7 @@ Toolkit.run(async tools => {
   console.log(tools.context.payload);
   await tools.github.pulls.createComment({
     pull_number: tools.context.payload.pull_request.number,
-    body: 'Hello world',
+    body: 'Hello world 2',
   });
 
   tools.exit.success('We did it team!');
