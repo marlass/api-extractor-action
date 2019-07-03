84c077655a47b5e57b892b9425e810970e65db07
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-03 11:43:44 +0200

    Fix console logs

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -1,14 +1,13 @@
 const { Toolkit } = require('actions-toolkit');
 
 Toolkit.run(async tools => {
-  tools.context.console.log(tools.context.payload);
+  // console.log(tools.context.payload);
   const config = {
     issue_number: tools.context.payload.pull_request.number,
     owner: tools.context.payload.repository.owner.login,
     repo: tools.context.payload.repository.name,
     body: 'Hello world 2',
   };
-  console.log(config);
   tools.store.set('prev', tools.getFile('etc/storefront.api.md'));
   tools.runInWorkspace('./scripts/api-extractor.sh');
   await tools.github.issues.createComment(config);
