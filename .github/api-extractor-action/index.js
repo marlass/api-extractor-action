7c55016b8bcd1fb1708086ac2cf2356bc4eb1a7f
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-03 08:14:23 +0200

    Add comment to PR

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -3,4 +3,9 @@ const tools = new Toolkit();
 
 console.log(tools.context.payload);
 
+tools.github.pulls.createComment({
+  pull_number: tools.context.payload.pull_request.number,
+  body: 'Hello world',
+});
+
 tools.exit.success('We did it team!');
