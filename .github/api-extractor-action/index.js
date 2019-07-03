b8d5c581adc3fc7df20346ec9742b36c4f4b9d52
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-03 11:35:32 +0200

    Test saving file

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -1,7 +1,7 @@
 const { Toolkit } = require('actions-toolkit');
 
 Toolkit.run(async tools => {
-  console.log(tools.context.payload);
+  tools.context.console.log(tools.context.payload);
   const config = {
     issue_number: tools.context.payload.pull_request.number,
     owner: tools.context.payload.repository.owner.login,
@@ -9,6 +9,8 @@ Toolkit.run(async tools => {
     body: 'Hello world 2',
   };
   console.log(config);
+  tools.store.set('prev', tools.getFile('etc/storefront.api.md'));
+  tools.runInWorkspace('./scripts/api-extractor.sh');
   await tools.github.issues.createComment(config);
   // Action code
 });
