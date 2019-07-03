b44f0cb1e853df19a50320a2d1acaa3520f9e218
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-03 08:19:11 +0200

    Change into async flow

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -1,11 +1,15 @@
 const { Toolkit } = require('actions-toolkit');
-const tools = new Toolkit();
 
-console.log(tools.context.payload);
+Toolkit.run(
+  async tools => {
+    console.log(tools.context.payload);
+    await tools.github.pulls.createComment({
+      pull_number: tools.context.payload.pull_request.number,
+      body: 'Hello world',
+    });
 
-await tools.github.pulls.createComment({
-  pull_number: tools.context.payload.pull_request.number,
-  body: 'Hello world',
-});
-
-tools.exit.success('We did it team!');
+    tools.exit.success('We did it team!');
+    // Action code
+  },
+  { event: 'push' }
+);
