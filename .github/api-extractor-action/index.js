350f69fbe1a27558aab25a09437e3358de24f64c
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-03 09:14:52 +0200

    Set correct owner

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -5,7 +5,7 @@ Toolkit.run(async tools => {
   await tools.github.pulls.createComment({
     pull_number: tools.context.payload.pull_request.number,
     commit_id: tools.context.sha,
-    owner: tools.context.payload.repository.owner.name,
+    owner: tools.context.payload.repository.owner.login,
     repo: tools.context.payload.repository.name,
     path: './README.md',
     position: 0,
