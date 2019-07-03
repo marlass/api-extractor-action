93ec32d2228dfb8a68019e38cc140a13424491b4
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-03 08:43:02 +0200

    Check commit sha

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -4,6 +4,7 @@ Toolkit.run(async tools => {
   console.log(tools.context.payload);
   await tools.github.pulls.createComment({
     pull_number: tools.context.payload.pull_request.number,
+    commit_id: tools.context.sha,
     body: 'Hello world 2',
   });
   // Action code
