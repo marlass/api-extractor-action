ef562efa822f5f7ee428bba5e3d0a4ab347ffa45
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-10 11:02:36 +0200

    test CI

--- scripts/api-extractor-for-develop.sh
+++ scripts/api-extractor-for-develop.sh
@@ -13,21 +13,21 @@ yarn
 yarn build:core:lib
 
 # Assets
-cp ./api/api-extractor-assets.json ./dist/assets/api-extractor.json
+cp ./../api/api-extractor-assets.json ./dist/assets/api-extractor.json
 (
   cd ./dist/assets && \
   api-extractor run --local --verbose
 )
 
 # Core
-# cp ./api/api-extractor-core.json ./dist/core/api-extractor.json
+# cp ./../api/api-extractor-core.json ./dist/core/api-extractor.json
 # (
 #   cd ./dist/core && \
 #   api-extractor run --local --verbose
 # )
 
 # Storefrontlib
-cp ./api/api-extractor-storefrontlib.json ./dist/storefrontlib/api-extractor.json
+cp ./../api/api-extractor-storefrontlib.json ./dist/storefrontlib/api-extractor.json
 (
   cd ./dist/storefrontlib && \
   api-extractor run --local --verbose
