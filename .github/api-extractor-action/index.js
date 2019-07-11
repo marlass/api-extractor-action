2de8efde07bc772a43f277632bc78ab0b9613dd5
Marcin Lasak <lasakmarcin90@gmail.com>
2019-07-11 12:36:06 +0200

    Add console log for debugging

--- .github/api-extractor-action/index.js
+++ .github/api-extractor-action/index.js
@@ -12,9 +12,9 @@ Toolkit.run(async tools => {
 
   const regex = /```ts\n(.*)```/gms;
 
-  const storefrontPRBranch = regex.exec(
-    tools.getFile('etc/storefront.api.md')
-  )[1];
+  const storefrontPRBranch = regex.exec(tools.getFile('etc/storefront.api.md'));
+
+  console.log(storefrontPRBranch);
   const assetsPRBranch = regex.exec(tools.getFile('etc/assets.api.md'))[1];
   await tools.runInWorkspace('sh', ['./scripts/api-extractor-for-develop.sh']);
   const storefrontTargetBranch = regex.exec(
